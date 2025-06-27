3d59e5b740d59fbbd777c010d3377d6c441829fe
2327cse1156 <anshkaushal92@gmail.com>
2025-06-27 22:51:37 +0530

    AUTHENTICATION

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-import { cache } from "react";
-import { buffer } from "stream/consumers";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 if (!MONGODB_URI) {
@@ -21,12 +19,10 @@ export async function dbConnect() {
   }
   if (!cached.promise) {
     const opts = {
-        bufferCommands:true,
-        maxPoolSize: 10,
-    }
-    mongoose
-    .connect(MONGODB_URI,opts)
-    .then(() => mongoose.connection)
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+    mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
   }
 
   try {
